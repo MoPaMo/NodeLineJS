3b7f2b27b7a3dd18f52b6990db108e4121a9cc31
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2024-06-28 13:55:29 +0200

    use dynamic input variabls

--- default_node.gd
+++ default_node.gd
@@ -1,25 +1,21 @@
 extends Panel
 
 # Declare variables for the LineEdit nodes
-@onready var line_edit_1 = $VBoxContainer/InOutSection/Ins/HBoxContainer/LineEdit
-@onready var line_edit_2 = $VBoxContainer/InOutSection/Ins/HBoxContainer2/LineEdit
-@onready var line_edit_3 = $VBoxContainer/InOutSection/Outs/HBoxContainer3/LineEdit
+@onready var input_boxes = $VBoxContainer/InOutSection/Ins.get_children().filter(lambdanode: node is HBoxContainer)
+@onready var output_boxes = $VBoxContainer/InOutSection/Outs.get_children().filter(lambdanode: node is HBoxContainer)
+
+#@onready var line_edit_3 = $VBoxContainer/InOutSection/Outs/HBoxContainer3/LineEdit
 
 func _ready():
 	# Connect the text_changed signal to custom methods using Callable
-	line_edit_1.text_changed.connect(Callable(self, "_on_line_edit_1_text_changed"))
-	line_edit_2.text_changed.connect(Callable(self, "_on_line_edit_2_text_changed"))
+	for input_box in input_boxes:
+		input_box.line_edit.text_changed.connect(Callable(self, "_on_line_edit_text_changed"))
 
 # Define the custom methods to handle the signal
-func _on_line_edit_1_text_changed(new_text: String) -> void:
+func _on_line_edit_text_changed(new_text: String) -> void:
 	print("Text in LineEdit 1 changed to: ", new_text)
 	_update_line_edit_3()
 
-func _on_line_edit_2_text_changed(new_text: String) -> void:
-	print("Text in LineEdit 2 changed to: ", new_text)
-	_update_line_edit_3()
-
-# Update LineEdit3 based on the values of LineEdit1 and LineEdit2
 func _update_line_edit_3() -> void:
 	var combined_text = "LineEdit1: %s, LineEdit2: %s" % [line_edit_1.text, line_edit_2.text]
 	line_edit_3.text = combined_text
